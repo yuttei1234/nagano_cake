c8bfb420af7aa43fbecba20ddb18509893ab27a3
EC2 Default User <ec2-user@ip-172-31-8-2.ec2.internal>
2021-09-13 12:09:30 +0000

    [revise]マイページ修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,34 @@
 class Public::CustomersController < ApplicationController
+  
+  before_action :authenticate_customer!, except: :top
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to public_customer_path, notice:"登録情報を変更しました"
+    else
+      render :edit
+    end   
+  end
+  
+  def destroy
+    customer = Customer.find(params[:id])
+    customer.destroy
+    redirect_to root_path
+  end
+  
+  private
+  
+  def customer_params
+    params.require(:user).permit(:last_name, :fisrt_name, :kata_last_name, :kata_fisrt_name, :email, :postal_code, :address, :telephone_number)
+  end
+  
 end
